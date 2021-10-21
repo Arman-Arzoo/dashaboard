d73acebb39038a500f9e4b1cc9d34f95f14dc55b
Arman Ali <51259303+Arman-Arzoo@users.noreply.github.com>
2021-10-21 15:17:28 +0500

    daily offer packages page

---
+++ src/components/services/DealOffer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import apic from "../services/dealimg.png";
+
+export default function DealOffer() {
+  return (
+    <div className="card ongoing-deal m-2 ">
+      <img src={apic} alt="Card" />
+      <p className="bg-dark label pl-2">Order no : 23221</p>
+      <p className="heading">Weekend Deal</p>
+      <p className="sub-heading pt-1">Service</p>
+      <p className="normal">Acrylic fill , French manicure , Nail Reshape</p>
+      <span className="pt-1">
+        <span className="sub-heading ">Originalprice :</span>
+        <span className="float-right pr-2">$150</span>
+      </span>
+      <span className="pt-1">
+        <span className="sub-heading">Discount : </span>
+        <span className="float-right pr-2">20%</span>
+      </span>
+      <span className="pt-1">
+        <span className="sub-heading">Discounted price : </span>
+        <span className="float-right pr-2">$135</span>
+      </span>
+      <span className="pt-1">
+        <span className="sub-heading">Duration : </span>
+        <span className="float-right pr-2">8/8/2021 - 11/8/2021</span>
+      </span>
+      {/* <Link to="#" className="btn btn-primary">
+            Go somewhere
+          </Link> */}
+    </div>
+  );
+}
--- src/pages/services/DailyDeal.jsx
+++ src/pages/services/DailyDeal.jsx
@@ -1,5 +1,138 @@
 import "./dailydeal.css";
+import Search from "../../components/home/search/search";
+import { Link } from "react-router-dom";
+import DealOffer from "../../components/services/DealOffer";
 
 export default function DailyDeal() {
-  return <div className="dailydeal">welcome to Services</div>;
+  return (
+    <div className="dailydeal">
+      <Search />
+      <span style={{ color: "#7A7A7A" }} className="pl-1">
+        Services/Deals
+      </span>
+      <div className="container-fluid pt-3 deal-container">
+        <div className="row">
+          <div className="col-4">
+            {/* template */}
+            <div className="customerDetails  ">
+              {/* card */}
+              <div className="card card-outline-secondary ">
+                {/* heading */}
+                <div className="card-header">
+                  <h3 className=" mb-0 ">Ongoing deal</h3>
+                </div>
+                {/* body */}
+                <div style={{ display: "flex" }}>
+                  {/* card for ongoing deals start */}
+                  <DealOffer />
+                  <DealOffer />
+                  {/* card for ongoing deals end */}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-8">
+            {/* template */}
+            <div className="customerDetails">
+              {/* card */}
+              <div className="card card-outline-secondary">
+                {/* heading */}
+                <div className="card-header">
+                  <h3 className=" mb-0 ">All Customers</h3>
+                </div>
+                {/* body */}
+                <div>body</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Deal pakages start  */}
+
+      {/* template */}
+      <div className="customerDetails pt-5">
+        {/* card */}
+        <div className="card card-outline-secondary">
+          {/* heading */}
+          <div className="card-header">
+            <h3 className=" mb-0 ">Deal Packages</h3>
+          </div>
+          {/* body */}
+          <div>
+            {/* carousel data start here */}
+            <div className="container">
+              <div className="row">
+                <div
+                  id="carousel"
+                  className="carousel slide"
+                  data-ride="carousel"
+                >
+                  {/* <ol className="carousel-indicators">
+                    <li
+                      data-target="#carousel"
+                      data-slide-to="0"
+                      className="active"
+                    ></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                  </ol> */}
+                  <div className="carousel-inner">
+                    <div className="carousel-item active ">
+                      <div className="d-none d-lg-block">
+                        <div className="slide-box">
+                          <DealOffer />
+                          <DealOffer />
+                          <DealOffer />
+                          <DealOffer />
+                          <DealOffer />
+                        </div>
+                      </div>
+                    </div>
+                    {/* second round */}
+                    <div className="carousel-item">
+                      <div className="d-none d-lg-block">
+                        <div className="slide-box">
+                          {/* items box here  start*/}
+                          <DealOffer />
+                          <DealOffer />
+                          <DealOffer />
+                          <DealOffer />
+                          <DealOffer />
+                          {/* items box end */}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <a
+                    className="carousel-control-prev"
+                    href="#carousel"
+                    role="button"
+                    data-slide="prev"
+                  >
+                    <span
+                      className="carousel-control-prev-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  <a
+                    className="carousel-control-next"
+                    href="#carousel"
+                    role="button"
+                    data-slide="next"
+                  >
+                    <span
+                      className="carousel-control-next-icon"
+                      aria-hidden="true"
+                    ></span>
+                    <span className="sr-only">Next</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* carousel data end here */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
