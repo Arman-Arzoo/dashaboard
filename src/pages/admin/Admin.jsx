4263279ace1d3cb80bb76d4332051eb6ad3c8ebd
Arman Ali <toplinegeeks@gmail.com>
2021-10-30 10:13:47 -0700

    Update Admin.jsx

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,10 +1,40 @@
+import Search from "../../components/home/search/search";
 import "./admin.css";
-import admincircle from "../../components/sidebar/icons/UserCircle.png";
+
 
 export default function Admin() {
   return (
-    <div className="admin">
-      <img src={admincircle} alt="" />
-    </div>
+    
+      
+      <div className="admin">
+        <Search />
+        <span style={{ color: "#7A7A7A" }} className="pl-1">
+          users/artists
+        </span>
+        {/* card */}
+        <div className="card card-outline-secondary mt-2 ">
+          {/* heading */}
+          <div
+            className="card-header"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <h3 className=" mb-0 ">List Of Admins</h3>
+            <button className="btn-add-artist">
+              <i className="fa fa-plus" aria-hidden="true"></i>
+              <span>Add new Admin</span>
+            </button>
+          </div>
+          {/* body */}
+          <div className="artist-card-body pl-4">
+          body area
+
+          </div>
+          
+        </div>
+      </div>
+    
   );
 }
