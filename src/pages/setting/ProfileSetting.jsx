86ab2ee01bdb29e51aad4533ad5ad54234017ce2
Arman Ali <toplinegeeks@gmail.com>
2021-11-01 02:55:23 -0700

    setting page update

--- src/pages/setting/ProfileSetting.jsx
+++ src/pages/setting/ProfileSetting.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import propic from "../setting/propic.png";
 export default function ProfileSetting() {
   return (
     <div>
@@ -12,7 +12,73 @@ export default function ProfileSetting() {
             <h3 className=" mb-0 ">Profile Settings</h3>
           </div>
           {/* body */}
-          <div>body</div>
+          <div>
+            <form style={{ display: "flex" }}>
+              <div className="setting-image-profile p-3 m-3">
+                <p>Change Profile Picture</p>
+                <img src={propic} alt="" />
+                <input type="file" name="" id="" />
+              </div>
+              <div>
+                <div className="form-row pt-4 pl-5">
+                  <div className="form-group col-md-5">
+                    <label for="inputsender">Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="name"
+                      placeholder="shailnay"
+                    />
+                  </div>
+                  <div className="form-group col-md-5">
+                    <label for="username">User Name</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="CardNumber"
+                      placeholder="shailnay cordway"
+                    />
+                  </div>
+                </div>
+
+                <div className="form-row pt-4 pl-5">
+                  <div className="form-group col-md-5">
+                    <label for="inputsender">Contact</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="contact"
+                      placeholder="shailnay"
+                    />
+                  </div>
+                  <div className="form-group col-md-5">
+                    <label for="username">Address</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="address"
+                      placeholder="shailnay cordway"
+                    />
+                  </div>
+                </div>
+
+                <div className="form-row pt-4 pl-5">
+                  <div className="form-group col-md-6">
+                    <label for="inputsender">Email</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      placeholder="@gmail.com"
+                    />
+                  </div>
+                  <div className="form-group col-md-4">
+                    <button className="btn">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
