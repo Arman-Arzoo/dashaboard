1c673bba7cedf4c63463aef94146fef6171e3bea
Arman Ali <toplinegeeks@gmail.com>
2021-11-04 01:18:07 -0700

    admin profile half complete

--- src/pages/admin/AdminProfile.jsx
+++ src/pages/admin/AdminProfile.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Search from "../../components/home/search/search";
 import "./adminprofile.css";
+import pic from "../admin/adminpic.png";
+import spic from "../admin/servicepic.png";
 export default function AdminProfile() {
   return (
     <div className="admin">
@@ -41,7 +43,44 @@ export default function AdminProfile() {
                   <h5 className=" mb-0 ">Most Recent Activity</h5>
                 </div>
                 {/* body */}
-                <div>dskfj</div>
+                <div className="p-4">
+                  <h5>New Artist Added</h5>
+                  <div className="admin-most-recent-activity">
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <img className="img" src={pic} alt="" />
+                      <div>
+                        <p>Tina Fox</p>
+                        <p>(123)242-2134</p>
+                        <p>tinafox@gmail.com</p>
+                      </div>
+                    </div>
+                    <p>view</p>
+                  </div>
+
+                  <h5>New Service Added</h5>
+                  <div className="admin-most-recent-activity">
+                    <div style={{ display: "flex", alignItems: "center" }}>
+                      <img className="simg" src={spic} alt="" />
+                      <div>
+                        <p>Acrylic paint and reshape </p>
+                        <p>$42</p>
+                      </div>
+                    </div>
+                    <p>view</p>
+                  </div>
+
+                  <h5 className="pt-2">Transfered Amount</h5>
+                  <div className="admin-most-recent-activity">
+                    <div>
+                      <h6>Tina Fox</h6>
+                      <div>
+                        <p>Acrylic paint and reshape </p>
+                        <p>$42</p>
+                      </div>
+                    </div>
+                    <p>view</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
