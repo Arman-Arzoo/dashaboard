94f4b5490fee9a79a26698ed03b58ec13c81f99f
Arman Ali <toplinegeeks@gmail.com>
2021-11-03 21:21:54 -0700

    Update ArtistProfile.jsx

--- src/pages/users/ArtistProfile.jsx
+++ src/pages/users/ArtistProfile.jsx
@@ -397,6 +397,100 @@ export default function ArtistProfile() {
             </div>
           </div>
         </div>
+
+        {/* Portfolio section start */}
+        <div className="ml-3 mr-3 mt-2">
+          <div className="card card-outline-secondary">
+            {/* heading */}
+            <div className="card-header">
+              <h3 className=" mb-0 ">Deal Packages</h3>
+            </div>
+            {/* body */}
+            <div>
+              {/* carousel data start here */}
+              <div className="container">
+                <div className="row">
+                  <div
+                    id="carousel"
+                    className="carousel slide"
+                    data-ride="carousel"
+                  >
+                    {/* <ol className="carousel-indicators">
+                    <li
+                      data-target="#carousel"
+                      data-slide-to="0"
+                      className="active"
+                    ></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                  </ol> */}
+                    <div className="carousel-inner">
+                      <div className="carousel-item active ">
+                        {/* <div className="d-none d-lg-block"> */}
+                        <div className="slide-box ">
+                          <p>fj</p>
+                          <p>fj</p>
+                          <p>fj</p>
+                          <p>fj</p>
+                        </div>
+                        {/* </div> */}
+                      </div>
+                      {/* second round */}
+                      <div className="carousel-item">
+                        {/* <div className="d-none d-lg-block"> */}
+                        <div className="slide-box">
+                          {/* items box here  start*/}
+
+                          <p>fj</p>
+                          <p>fj</p>
+                          <p>fj</p>
+                          <p>fj</p>
+                          {/* items box end */}
+                        </div>
+                        {/* </div> */}
+                      </div>
+                    </div>
+                    <a
+                      className="carousel-control-prev"
+                      href="#carousel"
+                      role="button"
+                      data-slide="prev"
+                    >
+                      <span
+                        className="carousel-control-prev-icon"
+                        aria-hidden="true"
+                      >
+                        <i
+                          className="fa fa-chevron-left"
+                          aria-hidden="true"
+                        ></i>
+                      </span>
+                      <span className="sr-only">Previous</span>
+                    </a>
+                    <a
+                      className="carousel-control-next"
+                      href="#carousel"
+                      role="button"
+                      data-slide="next"
+                    >
+                      <span
+                        className="carousel-control-next-icon"
+                        aria-hidden="true"
+                      >
+                        <i
+                          className="fa fa-chevron-right"
+                          aria-hidden="true"
+                        ></i>
+                      </span>
+                      <span className="sr-only">Next</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              {/* carousel data end here */}
+            </div>
+          </div>
+        </div>
+        {/* portfolio section end */}
       </div>
     </div>
   );
